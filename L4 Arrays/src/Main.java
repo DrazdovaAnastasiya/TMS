aa186fab0e021744a185fc7a39b1cb0d94e04ba9
DrazdovaAnastasiya <drozdovaanastasia638@gmail.com>
2023-01-04 16:23:08 +0300

    3 lesson

---
+++ L4 Arrays/src/Main.java
@@ -0,0 +1,95 @@
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {54, 5, 638, -7, 74, -45, -931, 11, 121, 1234554321};
+
+//        1. Вывести макс и мин числа
+
+        IntSummaryStatistics stats = Arrays.stream(arr).summaryStatistics();
+
+        System.out.println("Min element is " + stats.getMin());
+        System.out.println("Max element is " + stats.getMax());
+
+//        2. Вывести чет и не чет числа
+
+        System.out.print("Even numbers: ");
+
+        for (int j : arr) {
+            if (j % 2 == 0) {
+                System.out.print(j + " ");
+            }
+        }
+
+        System.out.print("\nOdd numbers: ");
+
+        for (int j : arr) {
+            if (j % 2 != 0) {
+                System.out.print(j + " ");
+            }
+        }
+
+//        3. Вывести самое длинное и короткое число
+
+        int maxLength = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int elementLength = Integer.toString(Math.abs(arr[i])).length();
+            if (elementLength > maxLength) {
+                maxLength = elementLength;
+            }
+        }
+
+        System.out.print("\nThe longest number is ");
+        for (int j : arr) {
+            int elementLength = Integer.toString(Math.abs(j)).length();
+            if (maxLength == elementLength) {
+                System.out.print(j + " ");
+            }
+        }
+
+        System.out.print("\nThe shortest number is ");
+        int minLength = 1;
+        for (int j : arr) {
+            int elementLength = Integer.toString(Math.abs(j)).length();
+            if (minLength == elementLength) {
+                System.out.print(j + " ");
+            }
+        }
+
+//        4. Выводить только те числа которые идут в порядке возрастания
+
+        System.out.println("\nNumbers in ascending order: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i < (arr.length - 1) && arr[i] < arr[i + 1]) {
+                System.out.print(arr[i] + ", ");
+            } else if (i >= 1 && arr[i - 1] < arr[i]) {
+                System.out.println(arr[i]);
+            }
+        }
+
+//        5. Вывести все числа палиндромы в массиве
+
+       System.out.print("Palindromes: ");
+
+        for (int j : arr) {
+            String str = Integer.toString(Math.abs(j));
+            if (str.length() == 1) {
+                continue;
+            }
+            boolean isPalindrome = false;
+            for (int i = 0; i <= (str.length() / 2); i++) {
+                if (str.charAt(i) == str.charAt(str.length() - 1 - i)) {
+                    isPalindrome = true;
+                } else {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            if (isPalindrome) {
+                System.out.print(j + " ");
+            }
+        }
+    }
+}
---
+++ L5 Arrays/src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        1. Создать дв. массив и проин. его числами от 1 до 100
+
+        int[][] matrix = new int[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                int value = i * 10 + j + 1;
+                matrix[i][j] = value;
+                System.out.printf("%2d ", value);
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+//        2. Проин. массив 1 в ввиде треугольника
+//           [1,0,0,0,0]
+//           [1,1,0,0,0]
+//           [1,1,1,0,0]
+//           [1,1,1,1,1]
+
+        int[][] matrix2 = new int[4][5];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                int value = 0;
+                if (j <= i) {
+                    value = 1;
+                }
+                matrix2[i][j] = value;
+                System.out.printf("%2d ", value);
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+//        3. Реализовать удобный вывод тругольника
+//        1
+//        11
+//        111
+//        1111
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (matrix2[i][j] > 0) {
+                    System.out.printf("%2d ", matrix2[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
